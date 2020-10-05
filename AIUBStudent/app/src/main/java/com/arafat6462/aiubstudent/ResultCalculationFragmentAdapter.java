7cb9998557205e1e407a44107732f6323f2c2d5f
Arafat Hossain <arafat6462@gmail.com>
2020-10-05 11:25:19 +0600

    fixed show/hide retake option ui on recyclerView

--- AIUBStudent/app/src/main/java/com/arafat6462/aiubstudent/ResultCalculationFragmentAdapter.java
+++ AIUBStudent/app/src/main/java/com/arafat6462/aiubstudent/ResultCalculationFragmentAdapter.java
@@ -1,6 +1,7 @@
 package com.arafat6462.aiubstudent;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,10 +22,6 @@ public class ResultCalculationFragmentAdapter extends RecyclerView.Adapter<Resul
     Context context;
     String[] title, description;
     int[] images;
-    ////////////////  show hide retake //////////////
-    LinearLayout linearLayout;
-    CheckBox checkBox;
-    ////////////////  show hide retake //////////////
 
     // for receive date from fragment layout class
     public ResultCalculationFragmentAdapter(Context context, String[] title, String[] description, int[] images) {
@@ -34,7 +31,12 @@ public class ResultCalculationFragmentAdapter extends RecyclerView.Adapter<Resul
         this.images = images;
     }
 
+
+
+
     // find the template layout-view from layout
+    // where to get the single card as view holder object
+    // what you want to show multiple time
     @NonNull
     @Override
     public ResultCalculationFragmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -47,18 +49,27 @@ public class ResultCalculationFragmentAdapter extends RecyclerView.Adapter<Resul
         return new ResultCalculationFragmentViewHolder(view);
     }
 
+
+
+
+
+
     // this onBindViewHolder method take data and set on template layout/row.
+    // if i found template card view then, what i need to do.
     @Override
     public void onBindViewHolder(@NonNull ResultCalculationFragmentViewHolder holder, int position) {
         // holder is the object of ResultCalculationFragmentViewHolder & carry all the information.
 
 //        holder.titleTextView.setText(title[position]);
-     //   holder.descriptionTextView.setText(String.valueOf(position)); // find the position and set the recycler view layout height according to position
+        //   holder.descriptionTextView.setText(String.valueOf(position)); // find the position and set the recycler view layout height according to position
         holder.descriptionTextView.setText(description[position]);
 //        holder.logoImageView.setImageResource(images[position]);
-
-
     }
+
+
+
+
+
 
     //this getItemCount is work for how many copy of template are made.
     @Override
@@ -67,32 +78,48 @@ public class ResultCalculationFragmentAdapter extends RecyclerView.Adapter<Resul
     }
 
 
+
+
+
     //////////////////////
     // managing the row and keep track
-    public class ResultCalculationFragmentViewHolder extends RecyclerView.ViewHolder {
+    public class ResultCalculationFragmentViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView titleTextView, descriptionTextView;
         ImageView logoImageView;
+        ////////////////  show hide retake //////////////
+        LinearLayout linearLayout;
+        CheckBox checkBox;
+        ////////////////  show hide retake //////////////
 
         // now all layout file convert to  view. and  this view came to ViewHolder class and now it is in itemView.
         // itemView is now your row template layout.
         public ResultCalculationFragmentViewHolder(@NonNull View itemView) {
             super(itemView);
-
             ////////////////  show hide retake //////////////
+
+            // itemView.setOnClickListener(this);
             checkBox = itemView.findViewById(R.id.checkBox2);
             linearLayout = itemView.findViewById(R.id.retake_LinearLayout_part);
+            linearLayout.setVisibility(View.GONE); // initial all gone
+            checkBox.setOnClickListener(this); // add click listener
 
+            ////////////////  show hide retake //////////////
+
+
+            //           titleTextView = itemView.findViewById(R.id.titleInTemplate);
+            descriptionTextView = itemView.findViewById(R.id.descriptionInTemplate);
+            //            logoImageView= itemView.findViewById(R.id.appLogoInTemplate);
+        }
+
+        @Override
+        public void onClick(View view) {
             if (checkBox.isChecked()) {
                 linearLayout.setVisibility(View.VISIBLE);
             } else {
                 linearLayout.setVisibility(View.GONE);
             }
-            ////////////////  show hide retake //////////////
 
-
- //           titleTextView = itemView.findViewById(R.id.titleInTemplate);
-            descriptionTextView = itemView.findViewById(R.id.descriptionInTemplate);
- //            logoImageView= itemView.findViewById(R.id.appLogoInTemplate);
+          //  Log.d("click from view holder", "onClick: ");
         }
     }
 }
